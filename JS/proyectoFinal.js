9b6e31d69af6022c56f76a69a97e80446758a5a0
josejimenez <josejimenez684@gmail.com>
2023-12-29 17:32:26 -0300

    entrega final

--- JS/proyectoFinal.js
+++ JS/proyectoFinal.js
@@ -1,23 +1,38 @@
 // crear el array de objetos //
 let carrito = [];
 
-const productosLista = [
-    { nombre: "Head Speed Mp", valor: 95000, descripcion: "Peso 300gr Tamaño de cabeza 100 inch Patron de cuerdas 16/19 Balance 300mm", img: "../imagenes/head speed.webp"},
-    { nombre: "Head Extreme Mp", valor: 90000, descripcion: "Peso 300gr Tamaño de cabeza 100 inch Patron de cuerdas 16/19 Balance 320mm", img: "../imagenes/head extreme.jpg" },
-    { nombre: "Head Radical Mp", valor: 110000, descripcion: "Peso 300gr Tamaño de cabeza 100 inch Patron de cuerdas 16/19 Balance 320mm", img: "../imagenes/head radical.jpg" },
-    { nombre: "Babolat Pure Aero", valor: 92000, descripcion: "Peso 300gr Tamaño de cabeza 100 inch Patron de cuerdas 16/19 Balance 320mm", img: "../imagenes/babolat puire aero.jpg" },
-    { nombre: "Wilson Pro staff", valor: 120000, descripcion: "Peso 290gr Tamaño de cabeza 97 inch Patron de cuerdas 16/19 Balance 325mm", img: "../imagenes/wilson pro staff.jpg" },
-    { nombre: "Wilson Clash", valor: 100000, descripcion: "Peso 310gr Tamaño de cabeza 100 inch Patron de cuerdas 16/19 Balance 310mm", img: "../imagenes/wilson clash.jpg" },
-    { nombre: "Bolso Yonex", valor: 120000, descripcion: "Peso 2kg Altura 35cm Ancho 35cm Cantidad de raquetas 6", img: "../imagenes/bolso yonex.jpg" },
-    { nombre: "Bolso Babolat", valor: 130000, descripcion: "Modelo Pure Aero Peso 1,600kg Altura 32cm Ancho 320cm", img: "../imagenes/bolso babolat.jpg" },
-    { nombre: "Bolso Head Novak", valor: 150000, descripcion: "Peso 2kg Altura 35cm Ancho 35cm Cantidad de raquetas 6", img: "../imagenes/bolos nole.jpg" },
-    { nombre: "Bolso Wilson", valor: 140000, descripcion: "Peso 2kg Altura 35cm Ancho 35cm Cantidad de raquetas 6", img: "../imagenes/bolso wilson.jpg" },
-    { nombre: "Tubo De Pelotas Prince", valor: 3500, descripcion: "Marca Prince Modelo NX Tour Pro Cantidad 3 pelotas", img: "../imagenes/tubo pelotas.jpg" },
-    { nombre: "Muñequera Nike", valor: 5600, descripcion: "Marca Nike Modelo Swoosh Cantidad 2", img: "../imagenes/muñequeras.jpg" },
-    { nombre: "Cubregrip Wilson", valor: 3200, descripcion: "Marca Wilson Modelo Ultra Wrap Comfort Cantidad 3", img: "../imagenes/cubre grip.jpg" },
-    { nombre: "Vincha Hydrogen", valor: 10000, descripcion: "Marca Hydrogen Material Algodón", img: "../imagenes/vincha.jpg" },
-    { nombre: "Antivibrador Head", valor: 500, descripcion: "Anti vibrador Head Logo", img: "../imagenes/anti vibrador.jpg" },
-];
+
+function obtenerProductos() {
+  return new Promise((resolve, reject) => {
+    fetch("../JSON/productosLista.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error al cargar la API");
+        }
+        return response.json();
+      })
+      .then(data => resolve(data))
+      .catch(error => reject(error));
+  });
+}
+
+// Función principal
+async function main() {
+  try {
+    // Obtener la lista de productos
+    const productosLista = await obtenerProductos();
+
+    // Mostrar los productos en la página
+    mostrarProductos(productosLista);
+  } catch (error) {
+    console.error("Error en la aplicación:", error);
+  }
+}
+
+// Llamar a la función principal
+main();
+
+
 
 function validarProductos(productosLista) {
   productosLista.forEach(producto => {
@@ -99,18 +114,32 @@ function finalizarCompra() {
 
 
 //BUSCAR PRODUCTOS//
-const productos = productosLista;
 
-function filtrarProductos(){
+// Función para filtrar productos
+async function filtrarProductos() {
+  try {
+    // Obtén la lista completa de productos
+    const productosLista = await obtenerProductos();
+
+    // Obtén el texto de búsqueda desde el campo de entrada
     const textoBusqueda = document.getElementById("buscadorProductos").value.toLowerCase();
-    
-    const productosFiltrados = productos.filter(producto => 
-        producto.nombre.toLowerCase().includes(textoBusqueda)
+
+    // Filtra los productos basándote en el texto de búsqueda
+    const productosFiltrados = productosLista.filter(producto =>
+      producto.nombre.toLowerCase().includes(textoBusqueda)
     );
-    mostrarProductos(productosFiltrados);   
+
+    // Muestra los productos filtrados en la interfaz
+    mostrarProductos(productosFiltrados);
+  } catch (error) {
+    console.error("Error al filtrar productos:", error);
   }
- //EVENTO PARA LA ENTRADA DEL USUARIO //
-  document.getElementById("buscadorProductos").addEventListener("input", filtrarProductos);
+}
+
+// Evento para la entrada del usuario
+document.getElementById("buscadorProductos").addEventListener("input", filtrarProductos);
+
+
 
 
 
@@ -141,8 +170,7 @@ function mostrarProductos(productosLista) {
   mostrarProductos(productosLista); 
 
 
-// formulario //
-
+// Formulario
 const formulario = document.getElementById("contacto");
 const btnEnviar = document.getElementById("enviar");
 
@@ -156,13 +184,11 @@ function validarFormularioContacto(e) {
   const mail = document.getElementById("mail").value.trim().toLowerCase();
   const mensaje = document.getElementById("mensaje").value.trim();
 
-  // Validar mensaje
   if (mensaje === "") {
     alert("Por favor, introduce tu mensaje.");
     return;
   }
 
-  // Validar mail
   if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
     alert("Por favor, introduce un correo electrónico válido.");
     return;
